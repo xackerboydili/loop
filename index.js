4b8407ed2f784da4ff304de9db43c16bf1ca9c10
xackerboydili <isoqovdilshod47@gmail.com>
2022-11-07 15:29:01 +0500

    last commit

---
+++ index.js
@@ -0,0 +1,332 @@
+// "1-chi masala"
+// function num(a, b){
+//     for (let i = 0; i < b; i++){
+//         console.log(a)
+//     }
+// }
+// num(6, 10)
+
+"2-chi masala"
+
+// function num(a, b){
+//     for(let i = a ; i <= b; i++){
+//         console.log(i)
+//     }
+
+// }
+// num(4, 30)
+
+"3-chi masala"
+
+// function num(a, b){
+//     for(let i = a; i > b; i--){
+//         console.log(i)
+//     }
+// }
+// num(30, 20)
+
+"4-chi masala"
+
+// function num(konfet){
+    
+//     for(i = 0; i <= 10; i++){
+//         console.log(i+'kg'+i * (konfet * 10000)+'sum')
+//     }
+// }
+// num(1)
+
+// "5-chi masala"
+
+// function num(){
+
+//     for(let i = 10; i <= 20; i+=2)
+//     console.log(i/10*10000)
+
+// }
+// num()
+
+"6-chi masala"
+
+// function num(konfet){
+//         for(let i = 1; i <= konfet; i++){
+//             console.log(i * (konfet * 10000))
+//         }
+// }
+// num(1.6)
+
+"7-chi masala"
+
+// function num(a, b){
+//     for(let i = a; i < b; i++){
+//         console.log(i + (a + b))
+//     }
+// }
+// num(1, 5)
+
+"8chi masala"
+
+// function num(a, b){
+//     for(i = 1; i < b; i ++){
+//         console.log(i * (a * b))
+//     }
+// }
+// num(2, 8)
+    
+
+"9-chi masala"
+
+// function num (a, b){
+//     for(i = 0; i < b; i ++){
+//         console.log(i * (a^2 + b^2))
+//     }
+// }
+// num(4, 10)
+
+"10-chi masala"
+
+// function num (n){
+//     for(let i = 1; i < n; i++){
+//         console.log(n = i/n + i/n)
+//     }
+// }
+// num(5)
+
+"11-chi masala"
+
+// function num(n){
+//     for(let i = 1; i < n; i++){
+//         console.log(i)
+//     }
+// }
+// num(27)
+
+"12-chi masala"
+
+// function num(n){
+//     for(let i = 0; i < n; i += 2){
+//         console.log(i)
+//     }
+// }
+// num(10)
+
+"13-chi masala"
+
+// function num(n){
+//     for(let i = 0; i < n; i ++ ){
+//         console.log(i / i)
+//     }
+// }
+// num(19)
+
+// "15-chi masala"
+
+// function num(n){
+//     for(let i = n; i > 0; i --){
+//         if(i % 2!==0 && i%3!==0&& i%5!==0&&i%7!==0){
+//             console.log(i)
+//         }
+        
+//     }
+    
+// }
+// num(100)
+
+"16-chi masala"
+
+// function num(n){
+//     for(let i = 0; i < n; n % 5 == 0, i += n){
+//         console.log(i)
+//     }
+// }
+// num(30)
+
+"17-chi masala"
+
+// function num(){
+
+// }
+
+
+// let all = function(a, b){
+//     let c = a+b;
+//     return c
+// }
+// console.log(all(10, 15))
+
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     phoneNumber:' asdasd'
+// };
+
+
+// (function () {
+//     console.log(person.firstName + ' ' + person.lastName);
+// })(person);
+
+
+
+// const Summa=(a, b)=>{
+//     let sum=0;
+//     for(let i=a; i<=b; i++){
+//         sum+=Math.pow(i, 2)
+//     }
+//     return sum
+// }
+// console.log(Summa(1, 10))
+
+
+
+// 18-masala
+// function Sum(n){
+//    let all=1
+// for(let i=1; i<=n; i++){
+//     for(let a=1; a<=i; a++){
+//         all*=a
+//         console.log(i+'chi'+all)
+//     }
+// }
+
+// }
+// Sum(10)
+
+
+"19-chi masala"
+
+// function calculateExponent(num, exp) {
+// 	return Math.pow(num, exp)
+// }
+// console.log(calculateExponent(5, 5))
+
+"20- masala"
+
+// function addUpTo(n) {
+//     let num = 0
+// 	for(let i = 1; i <= n; i ++){
+//         num += i
+//     }
+//     return num
+// }
+// console.log(addUpTo(4))
+
+"21-masala"
+
+// function addOddToN(n) {
+//     let num = ""
+// 	for(let i = 0; i <= n; i++){
+//         if(i % 2 !== 0){
+//             n.push(i);
+//         }
+//     }
+//     return num
+// }
+// addOddToN(10)
+
+"22-chi masala"
+
+// function findIndex(arr, str) {
+// 	return arr.indexOf(str)
+// }
+// console.log(findIndex( ["a", "g", "y", "d"], "d"))
+
+"23-chi masala"
+
+// function search(arr, item) {
+// 	for(let i = 0; i < arr.length; i++){
+//         if(arr[i] == item){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+// console.log(search([9, 8, 'red'], 'red'))
+
+"24-chi masala"
+
+//     function arrayValuesTypes(arr) {
+//         let newArray = [];
+//         for(let i=0; i<arr.length; i++){
+//             newArray.push(typeof arr[i]);
+//         }
+//         return newArray;
+//     }
+// console.log(arrayValuesTypes([true, 'boolean', 12]))
+
+
+
+
+
+// var arr = [1, 2, 3, 4, 5, 6]
+
+// var result = arr.splice(4, 0, "red")
+// console.log(result)
+
+"25-chi masala"
+// function length(s) {
+//     return s.lastIndexOf("")
+// }
+// console.log(length("Hello wolrd"))
+
+"26-chi masala"
+
+// function addIndexes(arr) {
+// 	let a = []
+//     for(let i = 0; i < arr.length; i++){
+//          a.push(arr[i] + i)
+//     }
+//     return a
+// }
+// console.log(addIndexes([0, 0, 0, 0, 0, 0]))
+
+"27-chi masala"
+
+// function squaresSum(n) {
+//     let num = 0 ;
+// 	for(let i = 0; i <= n; i++){
+//         num = num + i*i
+//     }
+//     return num
+// }
+// console.log(squaresSum(3))
+
+
+
+// var arr = [3, "2", 4, 2,  '344', 11, "6", 2, "654"]
+// var students = [ 
+//     { name: 'BMW', country: 'USA', age: 25 },
+//     { name: 'Bugatti', country: 'Italia',age :13 },
+//     { name: 'porsche', country: 'Poland', age: 19 },
+//     {name: 'Wolksvagen', country: 'UZB', age: 91 },
+//     { name: 'Damas', country: 'UZB', age: 82 },
+   
+// ]
+// let result = students.map(value=>`car ${value.name} country ${value.country} `)
+// let reduced = students.reduce((a, b)=>{
+//     return a+b.age
+// }, 20)
+// console.log(reduced)
+
+
+
+
+// var result = arr.find(2)
+
+// let result = students.find((students, index)=>{return  students.country==='Italia'})
+// let result1 = students.filter((students, index)=>{return students.country==='UZB'})
+// console.log('find', result)
+// console.log('filter', result1)
+
+
+
+
+
+
+// var filtered = students.filter(students=> students.surname==='Palonchiyev')
+// console.log(result)
+// console.log(filtered)
+// console.log(result1)
+
+let numbers = [1, 3, 5];
+let isEven = numbers.every(e => e % 2 == 0)
+
+console.log(isEven);
